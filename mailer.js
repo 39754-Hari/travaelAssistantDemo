9e502720a82c13d75d330c68bba3025bb2afa484
39754-Hari <39754.hexaware@gmail.com>
2018-04-26 14:11:39 +0530

    changes in mail

--- mailer.js
+++ mailer.js
@@ -2,41 +2,6 @@ var nodemailer 	= require('nodemailer');
 var fs 			= require('fs');
 var path		= require('path');	
 var mailer = {
-	mailTargetAudience:function(reqBody){
-		return new Promise(function(resolve, reject){
-		
-			var transporter = nodemailer.createTransport({
-				service: 'gmail',
-				auth: {
-					user: 'hexatestmailer@gmail.com',
-					pass: 'a###W14&$'
-				}
-			});
-			var fileName = 'Target-audience.xlsx';
-			var mailOptions = {
-			  from: 'hexatestmailer@gmail.com,harikrishnanv@hexaware.com',
-			  to: 'haritechno92@gmail.com',
-			  //to:'wilfredf@hexaware.com,saisub@hexaware.com,omprakashr@hexaware.com,arjunb@hexaware.com,ravindra.gharpure@gmail.com',
-			  subject:'Target Audience',
-			  html: 'Hi ,<div>Greetings from <b>TTC</b>!!</div><div style="margin-top:8px"> Please find attached the target audience for your area</div><div style="margin-top:8px">Regards,</div><div>TTC Team</div>',
-			  attachments:[
-				{
-					filename:fileName,
-					content:fs.createReadStream('Target-audience.xlsx')		
-				}
-			  ]
-			};
-
-			transporter.sendMail(mailOptions, function(error, info){
-				if (error) {
-					console.log(error);
-					reject(error);
-				} else {
-					resolve(reqBody);
-				}
-			});
-		});
-	}	,
 mailPackageDetails:function(){
 	return new Promise(function(resolve, reject){
 		var mailContent='';
